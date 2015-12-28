076e58adead3133250f7bb1d19f203deab100129
marceygema <marceygema@hotmail.com>
2015-12-28 07:39:35 +0100

    actualización  ui-route
    cambiamos el modulo ng-rotute por el mas moderno ui-route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,50 +8,62 @@
  *
  * Main module of the application.
  */
-angular
-  .module('ejecticioAngularJsApp', [
+
+
+// app.js
+var routerApp = angular.module('ejecticioAngularJsApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
     'ngSanitize',
     'ngTouch',
     'ui.bootstrap',
-    'ngGrid'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/empresa', {
-        templateUrl: 'views/empresa.html',
-        controller: 'EmpresaCtrl',
-        controllerAs: 'empresa'
-      })
-      .when('/contacto', {
-        templateUrl: 'views/contacto.html',
-        controller: 'ContactoCtrl',
-        controllerAs: 'contacto'
-      })
-      .when('/servicios', {
-        templateUrl: 'views/servicios.html',
-        controller: 'ServiciosCtrl',
-        controllerAs: 'servicios'
-      })
-      .when('/localizacion', {
-        templateUrl: 'views/localizacion.html',
-        controller: 'LocalizacionCtrl',
-        controllerAs: 'localizacion'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    'ngGrid',
+    'ui.router'
+  ]);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main'
+        })
+        
+        .state('empresa', {
+            url: '/empresa',
+            templateUrl: 'views/empresa.html',
+            controller: 'EmpresaCtrl',
+            controllerAs: 'empresa'       
+        })
+
+         .state('contacto', {
+            url: '/contacto',
+            templateUrl: 'views/contacto.html',
+            controller: 'ContactoCtrl',
+            controllerAs: 'contacto'       
+        })
+
+          .state('servicios', {
+            url: '/servicios',
+            templateUrl: 'views/servicios.html',
+            controller: 'ServiciosCtrl',
+            controllerAs: 'servicios'       
+        })
+
+         .state('localizacion', {
+            url: '/localizacion',
+            templateUrl: 'views/localizacion.html',
+            controller: 'LocalizacionCtrl',
+            controllerAs: 'localizacion'       
+        });
+        
+});
+
+
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,12 +28,11 @@ module.exports = function(config) {
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/ng-grid/build/ng-grid.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
